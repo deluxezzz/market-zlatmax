8d08135672c4e221cff962db974a94511996151f
deluxezzz <deluxez@mail.ru>
2022-07-23 00:42:29 +0700

    header-catalog

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 // alert(window.innerWidth);
+let popupButton = document.querySelector('.header-body__arrow');
+let popupMenu = document.querySelector('.header-body__popup');
+let header = document.querySelector('.header');
+
+if (popupButton){
+    popupButton.addEventListener('click', function(){
+        popupMenu.classList.toggle('_popup-active');
+        popupButton.classList.toggle('_popup-active');
+    });
+}
