fc4bbda8f49223f2ae659497751811dcc5cf05e0
deluxezzz <deluxez@mail.ru>
2022-07-25 02:05:14 +0700

    +burger

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// alert(window.innerWidth);
+//попап с телефонами
 let popupButton = document.querySelector('.header-body__arrow');
 let popupMenu = document.querySelector('.header-body__popup');
 let header = document.querySelector('.header');
@@ -9,3 +9,31 @@ if (popupButton){
         popupButton.classList.toggle('_popup-active');
     });
 }
+//меню-каталог
+let catalogLink = document.querySelectorAll('.catalog__link');
+let catalogMenu = document.querySelectorAll('.menu-catalog');
+let catalogWrapper = document.querySelectorAll('.catalog-wrapper');
+
+if (catalogLink){
+    for (let i = 0; i < catalogLink.length; i++){
+        catalogLink[i].addEventListener('click', function(event){
+            for (let y = 0; y < catalogLink.length; y++){
+                catalogLink[y].classList.remove('_active-link');
+                let openMenu = document.querySelectorAll('._open-menu');
+                if(openMenu.length > 0){
+                    catalogMenu[y].classList.remove('_open-menu');
+                }
+                
+            }
+            catalogLink[i].classList.toggle('_active-link');
+            catalogMenu[i].classList.toggle('_open-menu');
+            event.preventDefault();
+        });
+    }
+    
+}
+//бургер
+document.querySelector('.burger').addEventListener('click', function(){
+    document.querySelector('.burger span').classList.toggle('active');
+});
+document.querySelector('.menu').classList.toggle("animate");
